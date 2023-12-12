672c1c05fda42cd160e0aebb532239211e851d79
lofinif <ivan.bagaley@gmail.com>
2023-12-12 22:44:03 +0300

    refactoring api services

--- app/src/main/java/com/example/kinopedia/network/services/ApiService.kt
+++ app/src/main/java/com/example/kinopedia/network/services/ApiService.kt
@@ -2,36 +2,24 @@ package com.example.kinopedia.network.services
 
 import com.example.kinopedia.data.film.dto.ActorFilmPage
 import com.example.kinopedia.data.film.dto.KinopoiskFilm
+import com.example.kinopedia.data.filter.dto.Filters
 import com.example.kinopedia.data.home.dto.TopFilms
 import com.example.kinopedia.network.models.ExternalSources
 import com.example.kinopedia.network.models.FilmsByGenre
-import com.example.kinopedia.network.models.Filters
 import com.example.kinopedia.network.models.KinopoiskSimilarFilms
 import com.example.kinopedia.network.models.ThisMonthFilms
-import com.example.kinopedia.utils.BASE_URL
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-enum class LoadingStatus { LOADING, ERROR, DONE, DEFAULT }
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory()).build()
-
 val client = OkHttpClient.Builder().addInterceptor(Interceptor()).build()
 
-private val retrofit = Retrofit.Builder().client(client)
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL).build()
 
-class Interceptor: Interceptor {
+class Interceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val requestWithHeaders = request.newBuilder().header(
@@ -54,49 +42,57 @@ interface ApiService {
     suspend fun getPopularFilms(@Query("page") page: Int): TopFilms
 
     @GET("/api/v2.2/films/top?type=TOP_250_BEST_FILMS")
-    suspend fun getTopFilms(): TopFilms
+    suspend fun getTopFilms(
+        @Query("page") page: Int
+    ): TopFilms
 
     @GET("/api/v2.2/films/{id}")
     suspend fun getFilmById(
-        @Path("id") id : Int): KinopoiskFilm
+        @Path("id") id: Int
+    ): KinopoiskFilm
 
     @GET("/api/v2.2/films/{id}/similars")
     suspend fun getSimilarFilms(
-        @Path("id") id : Int): KinopoiskSimilarFilms
+        @Path("id") id: Int
+    ): KinopoiskSimilarFilms
 
     @GET("/api/v2.2/films/premieres")
     suspend fun getFilmsThisMonth(
         @Query("year") year: Int,
         @Query("month") month: String,
-        @Query("page") page: Int): ThisMonthFilms
+        @Query("page") page: Int
+    ): ThisMonthFilms
+
     @GET("/api/v1/staff")
     suspend fun getActorsAndStaff(
-        @Query("filmId") id: Int): List<ActorFilmPage>
+        @Query("filmId") id: Int
+    ): List<ActorFilmPage>
+
     @GET("/api/v2.2/films/filters")
     suspend fun getFilters(): Filters
+
     @GET("/api/v2.2/films")
     suspend fun getFilmByFilters(
         @Query("countries") countries: Array<Int>? = null,
         @Query("genres") genres: Array<Int>? = null,
-        @Query("order") order: String,
-        @Query("type") type: String,
+        @Query("order") order: String = "NUM_VOTE",
+        @Query("type") type: String = "FILM",
         @Query("keyword") keyword: String? = null,
-        @Query("ratingFrom") ratingFrom: Int,
-        @Query("ratingTo") ratingTo: Int,
-        @Query("yearFrom") yearFrom: Int,
-        @Query("yearTo") yearTo: Int,
+        @Query("ratingFrom") ratingFrom: Int = 7,
+        @Query("ratingTo") ratingTo: Int = 10,
+        @Query("yearFrom") yearFrom: Int = 2020,
+        @Query("yearTo") yearTo: Int = 2023,
         @Query("page") page: Int
     ): FilmsByGenre
 
     @GET("/api/v2.2/films/{id}/external_sources")
     suspend fun getExternalSources(
-        @Path("id") id: Int): ExternalSources
-    companion object FilmApi{
+        @Path("id") id: Int
+    ): ExternalSources
+
+    companion object FilmApi {
         fun create(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
     }
-}
-object FilmApi{
-    val retrofitService: ApiService by lazy { retrofit.create(ApiService::class.java) }
 }
 
 
--- app/src/main/java/com/example/kinopedia/network/services/ApiServiceOSM.kt
+++ app/src/main/java/com/example/kinopedia/network/services/ApiServiceOSM.kt
@@ -1,41 +1,27 @@
 package com.example.kinopedia.network.services
 
-import com.example.kinopedia.network.models.CityOSM
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.example.kinopedia.data.cinema.dto.CityOSM
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val BASE_URL_OSM = "https://nominatim.openstreetmap.org"
-private const val BASE_URL_OVERPASS = "https://overpass-api.de"
-
-private val moshiOSM = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory()).build()
+const val BASE_URL_OSM = "https://nominatim.openstreetmap.org"
 
 val interceptorOSM = HttpLoggingInterceptor()
 val clientOSM = OkHttpClient.Builder().addInterceptor(interceptorOSM).build()
-private val retrofitOSM = Retrofit.Builder().client(clientOSM)
-    .addConverterFactory(MoshiConverterFactory.create(moshiOSM))
-    .baseUrl(BASE_URL_OSM).build()
-
-private val retrofitOverpass = Retrofit.Builder().client(clientOSM)
-    .addConverterFactory(MoshiConverterFactory.create(moshiOSM))
-    .baseUrl(BASE_URL_OVERPASS).build()
 
 interface ApiServiceOSM {
     @GET("/reverse?format=json")
     suspend fun getCity(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double
-    ) : CityOSM
-}
+    ): CityOSM
 
-object OSMApi{
-    val retrofitService: ApiServiceOSM by lazy { retrofitOSM.create(ApiServiceOSM::class.java) }
+    companion object OsmApi {
+        fun create(retrofit: Retrofit): ApiServiceOSM = retrofit.create(ApiServiceOSM::class.java)
+    }
 }
 
 
